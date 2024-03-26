13e9935395da2bae0ebd3b54923d1655b742f276
Prabhat2373 <prabhattambe10@gmail.com>
2024-03-26 11:03:28 +0530

    seo changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,8 +9,24 @@ import MainLayout from "@/components/layout/MainLayout";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Youtube Downloader",
-  description: "Download youtube videos in High quality",
+  title: "Fast4k | Download High-Quality YouTube Videos & Audio ",
+  description:
+    "Discover the ultimate destination for high-quality YouTube video and audio downloads. Fast4k offers a seamless experience to access your favorite content in pristine quality. With our user-friendly interface, you can effortlessly download videos and audio tracks, ensuring a premium viewing and listening experience anytime, anywhere. Explore now and elevate your entertainment with our wide selection of content. Fast, reliable, and hassle-free downloads await you. Start downloading today!",
+  applicationName: "Fast4k",
+  referrer: "origin-when-cross-origin",
+  keywords: [
+    "Youtube Video Downloader 4k",
+    "youtube audio downloader",
+    "youtube 4k downloader ",
+  ],
+  // authors: [{ name: "Seb" }, { name: "Josh", url: "https://nextjs.org" }],
+  // creator: "Jiachi Liu",
+  // publisher: "Sebastian Markbåge",
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
 };
 
 export default function RootLayout({
@@ -22,8 +38,7 @@ export default function RootLayout({
     <html lang="en">
       <ColorSchemeScript />
       <body className={inter.className}>
-     
-          <MainLayout>{children}</MainLayout>
+        <MainLayout>{children}</MainLayout>
       </body>
     </html>
   );
