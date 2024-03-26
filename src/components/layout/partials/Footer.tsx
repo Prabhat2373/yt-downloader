13ea3a2cd06d5d877290f8259517ea58c528193f
Prabhat2373 <prabhattambe10@gmail.com>
2024-03-26 17:51:47 +0530

    changes

--- src/components/layout/partials/Footer.tsx
+++ src/components/layout/partials/Footer.tsx
@@ -6,8 +6,9 @@ import {
 } from "@tabler/icons-react";
 import { MantineLogo } from "@mantinex/mantine-logo";
 import classes from "./Footer.module.css";
+import Link from "next/link";
 
- function Footer() {
+function Footer() {
   return (
     <div className={classes.footer}>
       <Container className={classes.inner}>
@@ -18,28 +19,40 @@ import classes from "./Footer.module.css";
           justify="flex-end"
           wrap="nowrap"
         >
-          <ActionIcon size="lg" color="gray" variant="subtle">
-            <IconBrandTwitter
-              style={{ width: rem(18), height: rem(18) }}
-              stroke={1.5}
-            />
-          </ActionIcon>
-          <ActionIcon size="lg" color="gray" variant="subtle">
-            <IconBrandYoutube
-              style={{ width: rem(18), height: rem(18) }}
-              stroke={1.5}
-            />
-          </ActionIcon>
+          <Link href="https://twitter.com/code_prabhat" target="_blank">
+            <ActionIcon size="lg" color="gray" variant="subtle">
+              <IconBrandTwitter
+                style={{ width: rem(18), height: rem(18) }}
+                stroke={1.5}
+              />
+            </ActionIcon>
+          </Link>
+          <Link
+            href="https://www.youtube.com/channel/UC-N0_T4Qf0rUYr4CK0PHkwA"
+            target="_blank"
+          >
+            <ActionIcon size="lg" color="gray" variant="subtle">
+              <IconBrandYoutube
+                style={{ width: rem(18), height: rem(18) }}
+                stroke={1.5}
+              />
+            </ActionIcon>
+          </Link>
+          <Link
+            href="https://www.instagram.com/_prabhat_10/"
+            target="_blank"
+          >
           <ActionIcon size="lg" color="gray" variant="subtle">
             <IconBrandInstagram
               style={{ width: rem(18), height: rem(18) }}
               stroke={1.5}
             />
           </ActionIcon>
+          </Link>
         </Group>
       </Container>
     </div>
   );
 }
 
-export default Footer
+export default Footer;
