fc2b4fc19d9a14ab7ce1e220603977a32a51fb53
Prabhat2373 <prabhattambe10@gmail.com>
2024-03-28 17:01:28 +0530

    seo update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,15 @@ export const metadata: Metadata = {
     "youtube audio downloader",
     "youtube 4k downloader ",
   ],
+  twitter: {
+    card: "summary_large_image",
+    title: "Next.js",
+    description: "The React Framework for the Web",
+    siteId: "1467726470533754880",
+    creator: "@nextjs",
+    creatorId: "1467726470533754880",
+    images: ["https://nextjs.org/og.png"], // Must be an absolute URL
+  },
   // authors: [{ name: "Seb" }, { name: "Josh", url: "https://nextjs.org" }],
   // creator: "Jiachi Liu",
   // publisher: "Sebastian Markbåge",
@@ -26,6 +35,30 @@ export const metadata: Metadata = {
     email: false,
     address: false,
     telephone: false,
+  },
+  openGraph: {
+    title: "Fast4k | Download High-Quality YouTube Videos & Audio ",
+
+    description:
+      "Discover the ultimate destination for high-quality YouTube video and audio downloads. Fast4k offers a seamless experience to access your favorite content in pristine quality. With our user-friendly interface, you can effortlessly download videos and audio tracks, ensuring a premium viewing and listening experience anytime, anywhere. Explore now and elevate your entertainment with our wide selection of content. Fast, reliable, and hassle-free downloads await you. Start downloading today!",
+
+    url: "https://www.fast4k.com/",
+    siteName: "Fast4k",
+    // images: [
+    //   {
+    //     url: "https://nextjs.org/og.png", // Must be an absolute URL
+    //     width: 800,
+    //     height: 600,
+    //   },
+    //   {
+    //     url: "https://nextjs.org/og-alt.png", // Must be an absolute URL
+    //     width: 1800,
+    //     height: 1600,
+    //     alt: "My custom alt",
+    //   },
+    // ],
+    locale: "en_US",
+    type: "website",
   },
 };
 
