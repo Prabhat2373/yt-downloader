ed89deecf2ecca3a8a40bee49759c6437da27de8
Prabhat2373 <prabhattambe10@gmail.com>
2024-04-15 10:07:59 +0530

    dark mode changes

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -35,6 +35,23 @@ const theme = createTheme({
   //     }),
   //   }),
   // },
+  // black: "#000000",
+
+  colors: {
+    // override dark colors to change them for all components
+    dark: [
+      "#C1C2C5",
+      "#A6A7AB",
+      "#909296",
+      "#5C5F66",
+      "#373A40",
+      "#2C2E33",
+      "#25262B",
+      "#1A1B1E",
+      "#141517",
+      "#101113",
+    ],
+  },
 });
 
 const MainLayout = ({ children }) => {
