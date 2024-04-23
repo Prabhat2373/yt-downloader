76004d1a193ec35183268fdbd8ecc7088c972f8e
Prabhat2373 <prabhattambe10@gmail.com>
2024-04-23 15:30:43 +0530

    changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
       {
          name: 'my-app',
          script: 'npm run start',
-         cwd: '/Users/mrvautin/Documents/Code/my-app/',
+         cwd: '/Users/prabhattambe/Documents/github/yt-downloader',
          env: {
                NODE_ENV: 'development'
          },
@@ -11,15 +11,16 @@ module.exports = {
                NODE_ENV: 'production'
          }
       }
+      
    ],
    deploy: {
       production: {
          user: 'my-user',
          host: 'my-server',
-         key: '/Users/mrvautin/.ssh/id_rsa',
+         key: '/Users/prabhattambe/.ssh/id_rsa',
          ssh_options: 'ForwardAgent=yes',
          ref: 'origin/main',
-         repo: 'git@github.com:mrvautin/my-app.git',
+         repo: 'https://github.com/Prabhat2373/yt-downloader',
          path: '/var/www/html/my-app',
          'post-deploy': 'sh nextjs-pm2-deploy.sh'
       }
